6e6b123dec7804bff06569ea1cec559bc9fafcce
HariVeeraManiKumarVallu <107302465+HariVeeraManiKumarVallu@users.noreply.github.com>
2024-11-26 18:18:02 +0100

    remove unused function entry (#142)
    Co-authored-by: manik <manikumar046733@gmail.com>

--- homeassistant/components/abode/light.py
+++ homeassistant/components/abode/light.py
@@ -28,7 +28,7 @@ from .entity import AbodeDevice
 
 
 async def async_setup_entry(
-    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+    hass: HomeAssistant, async_add_entities: AddEntitiesCallback
 ) -> None:
     """Set up Abode light devices."""
     data: AbodeSystem = hass.data[DOMAIN]
